81fbea4da755959d75af8b66b6042970ff2c6cb6
lmosorio33-svg <lm.osorio33@gmail.com>
2025-10-28 20:07:16 -0600

    Día Cero: El inicio del cambio. (28-10-2025)

---
+++ constructor_silvermind_tracker.py
@@ -0,0 +1,7 @@
+# Importamos la libreria 'datatime'que nos da herramientas para trabajar con fechas y horas
+import datetime
+# Obtenemmos la fecha actual (solo la fecha, sin la hora)
+fecha_de_hoy= datetime.date.today()
+# Imprimimos esa fecha en la consola
+print("Iniciando Constructor Silvermind... ")
+print("Fecha del 'dia cero':", fecha_de_hoy)
